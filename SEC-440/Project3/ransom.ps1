02094f773757223a26f22bb65953073843408907
jwells24 <70913875+jwells24@users.noreply.github.com>
2023-02-07 11:34:02 -0500

    Update ransom.ps1

--- SEC-440/Project3/ransom.ps1
+++ SEC-440/Project3/ransom.ps1
@@ -1,1 +1,23 @@
+# Jack Wells, Encryption Ransomware, 2/7/2023, SEC-440
 
+# Create a random symmetric key, titled smem, given the public key we have already created outside of the scenario
+
+
+
+# Create an encrypted variant of this key, titled smem-enc
+
+
+
+# Read through a list of files in a directory and encrypt them, rename the file extension, and delete them
+
+
+
+# Delete smem, the original random symmetric key, and make sure to leave smem-enc
+
+
+
+# Using smem, prompt the user for verification and then decrypt their files
+
+
+
+# End of Script
